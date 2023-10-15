fa76fe4d9d083c568fa6fcdabb272a86be2f6c6e
thewrecker123 <105166080+thewrecker123@users.noreply.github.com>
2023-10-15 12:50:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+   
+
+  
+}
+
+
+
+
+
+function draw() {
+
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+
+}
+
+
+      
